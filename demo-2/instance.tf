590be3d9e2a84fb651729222c7fb88e63d3fd5e1
robin <xuhb@itshixun.com>
2024-11-10 23:37:22 +0800

    change error and add user data

--- demo-2/instance.tf
+++ demo-2/instance.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "demo-2" {
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
   key_name = aws_key_pair.mykeypair.key_name
 
+  user_data = templatefile("user_data.tftpl",{})
+
   tags = {
     Name = "demo-2"
   }
--- demo-2/vpc.tf
+++ demo-2/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "main-public-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = var.AWS_REGION
+  availability_zone       = "ap-east-1a"
 
   tags = {
     Name = "main-public-1"
@@ -25,7 +25,7 @@ resource "aws_subnet" "main-public-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = var.AWS_REGION
+  availability_zone       = "ap-east-1b"
 
   tags = {
     Name = "main-public-2"
