8133eb6fc9517e0ad81eeb0e4852cbed58a559b4
robin <xuhb@itshixun.com>
2024-11-10 22:43:12 +0800

    mod demo 2

--- demo-2/instance.tf
+++ demo-2/instance.tf
@@ -2,6 +2,10 @@ resource "aws_instance" "demo-2" {
   ami           = var.AMI
   instance_type = var.INSTANCE_TYPE
 
+  subnet_id = aws_subnet.main-public-1.id
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  key_name = aws_key_pair.mykeypair.key_name
+
   tags = {
     Name = "demo-2"
   }
---
+++ demo-2/output.tf
@@ -0,0 +1,3 @@
+output "instance" {
+  value = aws_instance.demo-2.public_ip
+}
