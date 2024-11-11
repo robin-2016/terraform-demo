f0c1392fc03eaaa3d7bffbd39e410b094b593a66
robin <xuhb@itshixun.com>
2024-11-11 18:20:44 +0800

    add demo 3

---
+++ demo-3/dev/instance.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "demo-3-dev"
+
+  instance_type          = "t3.micro"
+  key_name               = "mykeypair"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
